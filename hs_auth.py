025c2b384237279de45540b036a3ab85b99347be
Neha Batra <nehalita@gmail.com>
2013-04-21 15:46:13 -0400

    added dosctring for hs_auth.py

--- hs_auth.py
+++ hs_auth.py
@@ -1,21 +1,26 @@
 import urllib2
-from urllib import urlencode
+import urllib
 import json
+import sys
 
 def authenticate_with_hs(email, password):
-  base_link = "https://www.hackerschool.com/auth"
-  request = urllib2.Request(base_link)
+    """
+    Sends email and password to Hacker School's authentication page
+    If login is successful, returns user's information
+    Otherwise, returns "none"
+    """
+    base_link = "https://www.hackerschool.com/auth"
+    request = urllib2.Request(base_link)
 
-  data = {"email": email, "password": password}
-  request.add_data(urlencode(data))
+    data = {"email": email, "password": password}
+    request.add_data(urllib.urlencode(data))
 
-  try:
-    response = urllib2.urlopen(request)
-    return json.load(response)
-  except urllib2.HTTPError:
-    return None
+    try:
+        response = urllib2.urlopen(request)
+        return json.load(response)
+    except urllib2.HTTPError:
+        return None
 
 if __name__ == '__main__':
-  import sys
-  _, email, password = sys.argv
-  print authenticate_with_hs(email, password)
+    _, USER_EMAIL, USER_PASSWORD = sys.argv
+    print authenticate_with_hs(USER_EMAIL, USER_PASSWORD)
